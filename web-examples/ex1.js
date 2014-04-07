23e0c00ecc1a683874597d7a7c099d022a7e4a1e
David Wee <rook2pawn@gmail.com>
2014-04-07 00:27:27 -0700

    first commit

---
+++ web-examples/ex1.js
@@ -0,0 +1,74 @@
+var Chart = require('chart');
+var ee = require('events').EventEmitter;
+var through = require('through');
+var x = through(
+    function write(data) { 
+        this.queue(data);
+    },
+    function end() {
+        this.queue(null)
+    }
+);
+var windowsum = function(queue,k) {
+    var s = 0;
+    for (var i = queue.length - (2*k - 1); i < queue.length; i++) {
+        s += queue[i];
+    } 
+    return s;
+};
+var dsp = function(type) {
+    var queue = [];
+    this.filters = {
+        boxcar :through(
+            function write(data) {
+                queue.push(data);
+                if (queue.length >= 2*5-1) {
+                    queue[queue.length - 5] = windowsum(queue,5) / (2*5 - 1);
+                    this.queue(queue[queue.length - 5]);
+                }
+            }, 
+            function end() {
+                this.queue(null)
+            })
+    };
+    return this.filters.boxcar
+}
+x.tick = 0;
+setInterval(function() {
+    var sign = (Math.random() >= 0.5) ? 1 : -1;
+    var noise = sign*(Math.random() * 0.05);
+    x.write(Math.sin((x.tick/100)*Math.PI) + noise);
+    x.tick++;
+    if (x.tick == 100) 
+        x.tick = 0;
+}, 100);
+
+var chart = new Chart;
+var datasource = new ee;
+chart.color.line = ['#FF0000','#FF0000'];
+chart.series(datasource);
+chart.to(document.getElementById('c1'));
+
+var chart2 = new Chart;
+var datasource2 = new ee;
+chart2.color.line = ['#FF0000','#FF0000'];
+chart2.series(datasource2);
+chart2.to(document.getElementById('c2'));
+
+x.pipe(through(
+    function write(data) {
+        datasource.emit('data',{y:data});
+    },
+    function end() {
+        this.queue(null);
+    }
+));
+
+x.pipe(dsp('boxcar')).pipe(through(
+    function write(data) {
+        datasource2.emit('data',{y:data});
+    },
+    function end() {
+        this.queue(null);
+    }
+));
